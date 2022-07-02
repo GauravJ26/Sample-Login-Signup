7ba2c94019e21942b997d45f0a558c2754733df7
Gaurav Jaiswal <gaurav.rcet@gmail.com>
2022-07-02 21:11:09 +0530

    Update Aboutt.js

--- src/Aboutt.js
+++ src/Aboutt.js
@@ -6,8 +6,8 @@ function Aboutt() {
        <div className="card border-dark mx-5 my-5" >
   <div className="card-header border-dark bg-secondary"><h2> Company Info</h2></div>
   <div className="card-body text-dark">
-    <p className="card-text"> <strong>Company:</strong> Geeksynergy Technologies Pvt Ltd</p>
-    <p className="card-text"> <strong>Address:</strong> Sanjayanagar, Bengaluru-56</p>
+    <p className="card-text"> <strong>Company:</strong> XYZ Technologies Pvt Ltd</p>
+    <p className="card-text"> <strong>Address:</strong> Koramangala, Bengaluru-KA</p>
     <p className="card-text"> <strong>Phone:</strong> XXXXXXXXX09</p>
     <p className="card-text"> <strong>Email:</strong> XXXXXX@gmail.com</p>
   </div>
